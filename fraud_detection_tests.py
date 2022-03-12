279f261c107f6aed4975d6c6d7bc774ff91c96bb
liau11 <88518781+liau11@users.noreply.github.com>
2022-03-12 11:55:46 -0800

    added more tests

--- fraud_detection_tests.py
+++ fraud_detection_tests.py
@@ -3,14 +3,28 @@ import math
 
 
 def test_ones_and_tens_digit_histogram():
-    # example from spec
     actual = fd.ones_and_tens_digit_histogram([127, 426, 28, 9, 90])
     expected = [0.2, 0.0, 0.3, 0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.2]
     for i in range(len(actual)):
         assert math.isclose(actual[i], expected[i])
-    # add more test cases here
+    assert sum(expected) == 1.0
 
-# write other test functions here
+    actual = fd.ones_and_tens_digit_histogram([0, 0, 0])
+    expected = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    for i in range(len(actual)):
+        assert math.isclose(actual[i], expected[i])
+    assert sum(expected) == 1.0
+
+    actual = fd.ones_and_tens_digit_histogram([11, 11])
+    expected = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    for i in range(len(actual)):
+        assert math.isclose(actual[i], expected[i])
+    assert sum(expected) == 1.0
+
+    actual = fd.ones_and_tens_digit_histogram([])
+    expected = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    for i in range(len(actual)):
+        assert math.isclose(actual[i], expected[i])
 
 
 def main():
